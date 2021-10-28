a52f2e51d2d0634e9a6cdd752be292fde27db2c7
barrfalk <bfalk@salussystems.com>
2021-10-28 13:11:35 -0700

    Removed commas from personalisation body parameter since that appears to break GC Notify

--- common/mixins/common.js
+++ common/mixins/common.js
@@ -90,7 +90,7 @@ module.exports = function (Model, options) {
             phone_number: to,
             template_id: "059b4d59-1d9e-4218-92f6-e77be7df2809",
             personalisation: {
-              body_text: textBody,
+              body_text: textBody
             }
           };
           if (data && data.id) {
@@ -99,7 +99,7 @@ module.exports = function (Model, options) {
           await axios.post(url, body, {
             headers: {
               'Content-Type': 'application/json',
-              'Authorization': smsConfig[accountKey],
+              'Authorization': smsConfig[accountKey]
             },
           });
         } catch (ex) {
